60a9536e6e1959e64c74d506d745c61b2dcddf31
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-05-13 21:39:42 -0300

    create a test for verify if user can create a new transaction

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,10 +1,24 @@
-import { expect, test } from 'vitest'
+import { test, beforeAll, afterAll } from 'vitest'
+import { app } from '../app' // we let's have access the application without having to climb it
+import request from 'supertest'
 
-test('the user can create a new transaction', () => {
-  // here write our test
-  // here we let's do to called HTTP for create a new transaction
+beforeAll(async () => {
+  // this function execute before all tests
+  await app.ready() // we let's await that this app stay ready - let's await register all plugins because they are async
+})
 
-  const responseStatusCode = 201
+afterAll(async () => {
+  // this function execute after all tests
+  await app.close() // we let's await that this app stay close
+})
 
-  expect(responseStatusCode).toEqual(201) // here is what we let's await that happen - validation stage
+test('the user can create a new transaction', async () => {
+  await request(app.server) // we let's access 'Node' server http that are inside in framework
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
