83faa4deb5af2e1b95d6e66618999d0338a32705
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-05-17 18:16:54 -0300

    update project

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ app
   })
   .then(() => {
     // we let's execute a console.log when the promise finished your execution
-    console.log('HTTP Server Running!')
+    console.log(`HTTP Server running in port ${env.PORT}`)
   })
